1618f8e71b3efb3a32555c58301bbe7b08375d55
3n3a <info@3n3a.ch>
2022-02-23 10:25:01 +0100

    add aufgabe 2 & 3

---
+++ M226A/aufgabe2/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M226A_AP_00005_V2
+{
+    class A
+    {
+        public A()
+        {
+            Console.Write("A ");
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+            Console.Write("B ");
+        }
+    }
+
+    class C
+    {
+        private A mA;
+        private B mB;
+
+        public C()
+        {
+            mA = new A();
+            mB = new B();
+            Console.Write("C ");
+        }
+    }
+
+    class D : B
+    {
+        public D()
+        {
+            Console.Write("D ");
+        }
+    }
+
+    class E : C
+    {
+        private D mD;
+        public E()
+        {
+            mD = new D();
+            Console.Write("E ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            E mE = new E();
+        }
+    }
+}
