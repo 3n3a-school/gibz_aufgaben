03c3b64ec017e3a8d40b267d5d27ae81e5a36df3
3n3a <info@3n3a.ch>
2022-02-25 15:01:06 +0100

    add brainfuck

---
+++ M133/backend/src/a042L/index.php
@@ -0,0 +1,125 @@
+<?php
+
+$color = "#fefefe";
+$creds = [
+    "admin" => "admin",
+    "user" => "1234"
+];
+
+function checkCreds($username, $password) {
+    global $creds;
+    if (array_key_exists($username, $creds) && $creds[$username] == $password) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$auth_successful = false;
+
+if (array_key_exists('username', $_POST) && array_key_exists('password', $_POST)) {
+    $auth_successful = checkCreds($_POST["username"], $_POST["password"]);
+} else {
+    $auth_successful = false;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>a041L</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Comic+Neue&display=swap');
+        :root {
+            --animation-time: 0.1s;
+        }
+        body {
+            font-family: 'Comic Neue', cursive;
+        }
+        body, input, button, label {
+            font-size: 1.25rem;
+        }
+
+        html {
+            background: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);
+            background-size: 2800% 2800%;
+
+            -webkit-animation: Rainbow var(--animation-time) ease infinite;
+            -moz-animation: Rainbow var(--animation-time) ease infinite;
+            animation: Rainbow var(--animation-time) ease infinite;
+        }
+        h1, p, label, input, button {
+            color: rgb(255, 255, 255); 
+            mix-blend-mode: difference;
+            
+        }
+        @-webkit-keyframes Rainbow {
+            0%{background-position:0% 50%}
+            50%{background-position:100% 50%}
+            100%{background-position:0% 50%}
+        }
+        @-moz-keyframes Rainbow {
+            0%{background-position:0% 50%}
+            50%{background-position:100% 50%}
+            100%{background-position:0% 50%}
+        }
+        @keyframes Rainbow {
+            0%{background-position:0% 50%}
+            50%{background-position:100% 50%}
+            100%{background-position:0% 50%}
+        }
+        .main {
+            display: flex;
+            height: 95vh;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        .err-msg {
+            color: red;
+            font-weight: bold;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: .5em;
+        }
+        input {
+            background-color: transparent;
+            border: 3px solid rgb(255,255,255);
+            padding: .5em .75em;
+        }
+        button {
+            background-color: transparent;
+            border: 3px solid rgb(255,255,255);
+            padding: .5em .75em;
+        }
+        .field {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
+</head>
+<body>
+    <div class="main">
+        <h1>Login</h1>
+        <form action="./" method="POST">
+            <div class="field">
+                <label for="username">Username</label>
+                <input type="text" name="username" placeholder="Enter your Username...">
+            </div>
+            <div class="field">
+                <label for="password">Password</label>
+                <input type="password" name="password">
+            </div>
+            
+            <button>Login</button>
+        </form>
+
+        <?= $auth_successful ? "Lmao secret" : "Please enter a password" ?>
+    </div>
+</body>
+</html>
