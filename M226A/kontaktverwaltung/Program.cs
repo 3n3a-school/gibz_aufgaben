38e0b4097c98ccb3ee3976507965789e5d45fcc2
3n3a <info@3n3a.ch>
2022-03-02 08:40:13 +0100

    create object dumper

--- M226A/kontaktverwaltung/Program.cs
+++ M226A/kontaktverwaltung/Program.cs
@@ -1,85 +1,109 @@
 ﻿using System;
-using System.Reflection;
+using System.ComponentModel;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace ContactManagment
 {
-    enum Gender
+    public enum Gender
     {
         Male,
         Female,
         Other
     }
 
-    enum PhoneNumType {
+    public enum PhoneNumType {
         Mobile,
         Landline
     }
 
-    class ContactType
+    public enum ContactTypes {
+        Phonenumber,
+        Email,
+        Address
+    }
+
+    public class Phonenumber {
+        public string Number {get;set;}
+        public PhoneNumType Type {get;set;}
+    }
+
+    public class Email
     {
-        int PrimaryOptionIndex {get;set;}
+        public string EmailAddress {get;set;}
     }
 
-    class Phonenumber : ContactType {
-        string Number {get;set;}
-        PhoneNumType Type {get ;set;}
-    }
-
-    class Email : ContactType
+    public class Address
     {
-        string EmailAddress {get;set;}
+        public string Street {get;set;}
+        public string Postcode {get;set;}
+        public string City {get;set;}
     }
 
-    class Address : ContactType
-    {
-        string Street {get;set;}
-        string Postcode {get;set;}
-        string City {get;set;}
-    }
+    public class ContactData {
+        public ContactTypes PrimaryOption {get;set;}
+        public int PrimaryOptionIndex {get;set;}
 
-    class ContactData {
-        List<Email> Emails {get;set;}
-        List<Phonenumber> Phonenumbers {get;set;}
-        List <Address> Addresses {get;set;}
+        public List<Email> Emails {get;set;}
+        public List<Phonenumber> Phonenumbers {get;set;}
+        public List <Address> Addresses {get;set;}
     }
 
     public class Person
     {
-        string FirstName {get; set;}
-        string LastName {get;set;}
-        DateTime Birthday {get;set;}
-        Gender Gender {get;set;}
-        ContactData ContactOptions {get;set;}
+        public string FirstName {get; set;}
+        public string LastName {get;set;}
+        public DateTime Birthday {get;set;}
+        public Gender Gender {get;set;}
+        public ContactData ContactOptions {get;set;}
     }
 
     public class InternalPerson : Person
     {
-        string Department { get;set; }
-        DateTime StartingDate {get; set;}
+        public string Department { get;set; }
+        public DateTime StartingDate {get; set;}
     }
 
     public class ExternalPerson : Person {
-        string Company {get;set;}
+        public string Company {get;set;}
     }
 
     public class Program
     {
-        public void PrintPersonInfos(Person p) {
-            string[] fields = new string[] {
-                "FirstName",
-                "LastName"
-            };
-
-            foreach (string field in fields)
-            {
-                Console.WriteLine($"{field}: {p.GetField(field)}");
-            }
-        }
+        public static List<Person> Contacts = new List<Person>();
         public static void Main(string[] args) {
-            InternalPerson internalPerson = new();
-            ExternalPerson externalPerson = new();
-            PrintPersonInfos(internalPerson);
+            Person p = new Person() {
+                FirstName = "Hans",
+                LastName = "Zimmer",
+                Birthday = new DateTime(2020, 12, 1),
+                Gender = Gender.Female,
+                ContactOptions = new ContactData() {
+                    PrimaryOption = ContactTypes.Email,
+                    PrimaryOptionIndex = 0,
+                    Addresses = new List<Address>() {
+                        new Address() {
+                            City = "Lucerne",
+                            Postcode = "6005",
+                            Street = "Mainstreet 1"
+                        }
+                    },
+                    Emails = new List<Email>() {
+                        new Email() {
+                            EmailAddress = "test@enea.tech"
+                        }
+                    },
+                    Phonenumbers = new List<Phonenumber>() {
+                        new Phonenumber() {
+                            Number = "+417942334400",
+                            Type = PhoneNumType.Mobile
+                        }
+                    }
+                }
+            };
+            Contacts.Add(p);
+            Console.WriteLine(
+                ObjectDumper.Dump(Contacts[0])
+            );
         }
     }
 }
