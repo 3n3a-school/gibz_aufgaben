2b45202376f3c1383718e0b1a5dd4d94a555cb17
Enea Krähenbühl <enk@suva.ch>
2022-03-07 16:32:28 +0100

    create tree view for file structure

--- M133/backend/src/a001l/index.php
+++ M133/backend/src/a001l/index.php
@@ -1,17 +1,54 @@
 <?php
 
+/**
+ * Recursive directory listing for given path
+ */
 function ls($path=__DIR__) {
     $list = scandir($path);
+    $out_list = [];
 
     foreach ($list as $i => $list_el) {
-        
+        if ( $list_el != "." && $list_el != ".." ) {
+            $path_to_el = $path . '/' . $list_el;
+
+            if ( is_dir($path_to_el) ) {
+                $subdirs = ls($path_to_el);
+                array_push($out_list, [
+                    'name' => $list_el,
+                    'dirs' => $subdirs
+                ]);
+            } else {
+                // is file
+                array_push($out_list, [
+                    'name' => $list_el,
+                ]);
+            }
+        }
     }
 
-    return print_r($list, true);
-} 
+    return $out_list;
+}
+
+function tree_view_html($array) {
+    $out = "<ul>";
+
+    foreach ($array as $key => $value) {
+        if (array_key_exists('dirs', $value)) {
+            $out .= "<li> - ".$value['name'];
+            $out .= tree_view_html($value['dirs']);
+            $out .= "</li>";
+        } else {
+            $out .= "<li>".$value['name']."</li>";
+        }
+    }
+
+    $out .= "</ul>";
+
+    return $out;
+}
 
 ?>
 
 <pre>
-<?= ls("..") ?>
+<?= tree_view_html(ls("..")) ?>
 </pre>
