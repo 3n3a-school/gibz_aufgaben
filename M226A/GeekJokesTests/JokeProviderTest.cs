6acc3c2cd0f7624611d47803b21131a93240062e
3n3a <3n3a@enea.software>
2022-03-09 14:32:08 +0100

    geek-jokes m226a: init

---
+++ M226A/GeekJokesTests/JokeProviderTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GeekJokesTests
+{
+    public class JokeProviderTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ M226A/geek-jokes/Program.cs
@@ -0,0 +1,33 @@
+﻿using GeekJokes.Models;
+using GeekJokes.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace GeekJokes
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            JokeService jokeService = new JokeService();
+            bool printMoreJokes;
+
+            System.Console.Clear();
+            
+            do
+            {
+                Joke joke = await jokeService.RetrieveAndAnalyzeJoke();
+                Console.WriteLine(joke);
+
+                Console.Write("\n=> Would you like to enjoy one more joke? [Y/n] ");
+                var key = Console.ReadKey();
+                Console.WriteLine("\n");
+                printMoreJokes = key.KeyChar.ToString().ToLower() != "n";
+                if (printMoreJokes)
+                {
+                    Console.Clear();
+                }
+            } while (printMoreJokes);
+        }
+    }
+}
