696754732a7ce519ef9433ba66778ee077357736
3n3a <info@3n3a.ch>
2022-03-09 09:20:58 +0100

    update m226a testing

--- M226A/NumberConverterTest/NumberConverterTest.cs
+++ M226A/NumberConverterTest/NumberConverterTest.cs
@@ -2,10 +2,36 @@ using Xunit;
 using FluentAssertions;
 using UnitTestingBasics;
 
+using System.Collections;
+using System.Collections.Generic;
+
 namespace NumberConverterTest;
 
 // Fact: parameterless test
 // Theory: parametrised, test
+
+class RoundDownDataGenerator : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { 12.4, 12};
+        yield return new object[] { -1.2, -1 };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
+
+class RoundToPowerOfTenDataGenerator : IEnumerable<object[]>
+{
+    // poweroften, input, expected
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { 2, 50, 100 };
+        yield return new object[] { 2, 10, 0 };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
 
 public class RoundingTest
 {
@@ -17,16 +43,24 @@ public class RoundingTest
     [Theory]
     [InlineData(12.5f, 13)]
     [InlineData(-1, -1)]
+    [InlineData(float.MinValue, -3)]
     [InlineData(int.MinValue, int.MinValue)]
-    public void RoundUp(int numberNow, int numberRounded)
+    public void RoundUp(float numberNow, int numberRounded)
     {
         nc.RoundUp(numberNow)
             .Should().Be(numberRounded);
     }
 
-    [Fact]
-    public void RoundDown()
+    [Theory]
+    [ClassData(typeof(RoundDownDataGenerator))]
+    public void RoundDown(float input, int expected)
     {
-       nc.RoundDown(12.5f).Should().Be(12);
+       nc.RoundDown(input).Should().Be(expected);
+    }
+
+    [Theory]
+    [ClassData(typeof(RoundToPowerOfTenDataGenerator))]
+    public void RoundToPowerOfTen(int powerOfTen, float input, int expected) {
+        nc.RoundToPowerOfTen(input, powerOfTen).Should().Be(expected);
     }
 }
